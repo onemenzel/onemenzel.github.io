f46e096bd5767b6c010421e87e48d245dbbfd3db
Lukas Menzel <lukas@menzel.earth>
2020-11-26 20:53:56 +0100

    first version of todo mvc collab

---
+++ todo-mvc-collab/precache-manifest.98e67c5fbb83e59c0096b3d10d5fee4d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a20c6d8266ad164b39e9",
+    "url": "/css/app.ca36ff6a.css"
+  },
+  {
+    "revision": "19a5935da8141d89b42b2a8f2c7873b2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a20c6d8266ad164b39e9",
+    "url": "/js/app.81e490a8.js"
+  },
+  {
+    "revision": "3db5ea216529d1d2e96c",
+    "url": "/js/chunk-vendors.d3328ba7.js"
+  },
+  {
+    "revision": "f6eba3ecbcb332c31fdc43996db0a3b9",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
---
+++ todo-mvc-collab/service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/precache-manifest.98e67c5fbb83e59c0096b3d10d5fee4d.js"
+);
+
+workbox.core.setCacheNameDetails({prefix: "vue3-yjs-todo"});
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
